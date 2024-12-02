01d00eac21bc7127ee5a633160446ee6d69e2b4b
yanrulai <yanrulai@nexcobot.com>
2024-12-02 17:35:43 +0800

    add rt task

--- src/ecm_rt.cpp
+++ src/ecm_rt.cpp
@@ -5,21 +5,24 @@
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <alchemy/task.h>
 
 NEC_RtCyclicCallback callbackFunc;
 
 // Xenomai task
-// RT_TASK ecmTaskHandle;
+RT_TASK ecmTaskHandle;
 
 void registerCallback(NEC_RtCyclicCallback cb){
     callbackFunc = cb;
 }
 
 void scanLoop() {
+    RT_TASK_INFO taskInfo;
     while (true) {
         std::cout << "Start task : \n"<< std::endl;
-        std::cout << "Task name: ScanTask\n"<< std::endl;
-        auto regs = scanPCI();        
+        rt_tasl_inquire(NULL, &taskInfo);
+        std::cout << "Task name: " << taskInfo.name  << "\n"<< std::endl;
+        auto regs = scanPCI();
         for (int reg : regs) {
             int value = readRegister(reg);
             if (callbackFunc) {
@@ -27,11 +30,19 @@ void scanLoop() {
             }
         }
         std::cout << "End task. \n\n"<< std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // 模拟周期性扫描
+        rt_task_sleep(1000000000);  // 1 秒
     }
 }
 
 void startECM() 
 {
-    std::thread(scanLoop).detach(); // 启动周期性扫描线程
+    // 创建 Xenomai 实时任务
+    int err = rt_task_create(&ecmTaskHandle, "ScanTask", 0, 99, T_JOINABLE);
+    if (err) {
+        std::cerr << "Error creating real-time task: " << strerror(-err) << std::endl;
+        return;
+    }
+
+    // 启动任务
+    rt_task_start(&ecmTaskHandle, &scanLoop, NULL);
 }
